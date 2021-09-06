00977196a7859c95c2dc257fc471f5ac619a6889
ekateryna-rodina <work.rodina@gmail.com>
2021-09-06 18:03:17 -0400

    mobile menu is added

--- src/js/index.js
+++ src/js/index.js
@@ -12,14 +12,21 @@
     if (open) {
       overlay.classList.add("fadeIn");
       overlay.classList.remove("fadeOut");
+
+      popupMenu.classList.add("fadeIn");
+      popupMenu.classList.remove("fadeOut");
     } else {
       overlay.classList.add("fadeOut");
       overlay.classList.remove("fadeIn");
+
+      popupMenu.classList.add("fadeOut");
+      popupMenu.classList.remove("fadeIn");
     }
   }
   const links = document.querySelectorAll(".header__links>li");
   //   toggle bars
   const menuBars = document.getElementById("toggleMenu");
+  const popupMenu = document.getElementById("popupMenu");
   links.forEach((l) => l.addEventListener("click", onActiveMenu, true));
   menuBars.addEventListener("click", toggleBars, true);
 })();
